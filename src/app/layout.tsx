c4b760badc55e62bc9fefd5ceaee85eb90b4d111
Nithin-Varma <mnithin1422@gmail.com>
2024-07-22 02:48:18 +0530

    basic hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <ThemeProvider attribute="class">
       <body className={inter.className}>{children}</body>
+      </ThemeProvider>
     </html>
   );
 }
---
+++ src/components/landing/backgroundBoxes.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
+  const rows = new Array(50).fill(1);
+  const cols = new Array(30).fill(1);
+
+  return (
+    <div
+      style={{
+        transform: `translate(-40%,-60%) skewX(0deg) skewY(0deg) scale(0.675) rotate(0deg) translateZ(0)`,
+      }}
+      className={cn(
+        "absolute left-1/4 p-4 -top-1/4 flex w-full h-full z-0 ",
+        className
+      )}
+      {...rest}
+    >
+      {rows.map((_, i) => (
+        <motion.div
+          key={`row` + i}
+          className="w-12 h-12  border-r-full relative"
+        >
+          {cols.map((_, j) => (
+            <motion.div
+              whileHover={{
+                backgroundColor: "whitesmoke",
+                transition: { duration: 0 },
+              }}
+              animate={{
+                transition: { duration: 2 },
+              }}
+              key={`col` + j}
+              className="w-12 h-12  border-r border-t  relative"
+            >
+          
+            </motion.div>
+          ))}
+        </motion.div>
+      ))}
+    </div>
+  );
+};
+
+// export const Boxes = React.memo(BoxesCore);
